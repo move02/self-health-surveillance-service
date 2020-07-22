0dd6d19fed7084a698c4a1228e3ab6643616e621
move02 <ehd223@naver.com>
2020-07-22 22:44:20 +0900

    add revision

---
+++ migrations/versions/ae724ae1274f_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ae724ae1274f
+Revises: 
+Create Date: 2020-07-22 22:44:07.362296
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ae724ae1274f'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
